0a5594b1bac084360c891b33f9b107b83770c44f
Nathan Adams <natefromnorthgate@gmail.com>
2017-11-29 09:56:13 -0700

    added function to hamburger menu. Pre float.

--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -1,4 +1,27 @@
 
+
+// Establishing Global Variables
+
+var menuActive = false;
+
+
+
+// Hamburger Menu Triggers and code
+
+$(document).ready(function(){
+  menuActive = false;
+  $('.menu-wrap').css('display','none');
+});
+
 $('.hamburger-menu').on('click', function() {
-	$('.bar').toggleClass('animate');
+  if (menuActive === false) {
+    $('.menu-wrap').css('display','block');
+    menuActive = true;
+  } else if (menuActive === true) {
+    $('.menu-wrap').css('display','none');
+    menuActive = false;
+  } {
+
+  }
+  $('.bar').toggleClass('animate');
 });
